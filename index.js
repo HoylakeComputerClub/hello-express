517e3505b7a44162519fae93a7519c917e92b8f0
Tom Tarpey <tomtarpeydev@gmail.com>
2023-04-26 17:21:23 +0100

    added netlify lambda

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-app.get('/', (req, res) => res.json({"msg": "hello from the api"}))
-app.listen(3004);
